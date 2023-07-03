158e138b2fad25492a1f7c7ed96bccce0340f755
UuBroot <lolylyt@gmail.com>
2023-07-03 18:05:30 +0200

    update babylon

--- main.js
+++ main.js
@@ -4,39 +4,96 @@ new TypeIt("#title", {
     waitUntilVisible: true,
 }).go();
 
-const canvas = document.getElementById("background");
-const engine = new BABYLON.Engine(canvas, true, { preserveDrawingBuffer: true, stencil: true });
+//Events
+//document.addEventListener("mousemove", mouseMoveHandler, false);
 
-// GLOBAL OBJECTS
-let sphere;
-let sphere2;
-class Playground {
-    static CreateScene(engine, canvas) {
-        // SCENE
-        let scene = new BABYLON.Scene(engine);
-        // CAMERA
-        let camera = new BABYLON.UniversalCamera("Camera", new BABYLON.Vector3(0, 1, 15), scene);
-        camera.setTarget(new BABYLON.Vector3(0, 1, 0));
-        camera.attachControl(canvas, true);
-        camera.speed = 0.2;
-        camera.keysUp[0] = "W".charCodeAt(0);
-        camera.keysDown[0] = "S".charCodeAt(0);
-        camera.keysLeft[0] = "A".charCodeAt(0);
-        camera.keysRight[0] = "D".charCodeAt(0);
-        camera.keysRotateLeft[0] = "37".charCodeAt(0);
-        camera.keysRotateRight[0] = "39".charCodeAt(0);
-        camera.keysUpward[0] = "32".charCodeAt(0);
-        camera.keysDownward[0] = "17".charCodeAt(0);
-        camera._needMoveForGravity = true;
-        return scene;
-    }
+
+
+let camera;
+// Babylon
+if (BABYLON.Engine.isSupported()) {
+    var canvas = document.getElementById("background");
+    var engine = new BABYLON.Engine(canvas, true);
+    var scene = new BABYLON.Scene(engine);
+
+    camera = new BABYLON.UniversalCamera("UniversalCamera", new BABYLON.Vector3(0, 0, 20), scene);
+    let sun = new BABYLON.PointLight("Omni0", new BABYLON.Vector3(60, 100, 10), scene);
+
+    /*
+    const animation = new BABYLON.Animation(
+        "cameraRotation",
+        "position",
+        30,
+        BABYLON.Animation.ANIMATIONTYPE_VECTOR3,
+        BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE
+    );
+      
+      // Define keyframes for the animation
+      const keys = [];
+      keys.push({
+        frame: 0,
+        value: new BABYLON.Vector3(0, 0, 20)
+      });
+      keys.push({
+        frame: 100,
+        value: new BABYLON.Vector3(Math.PI * 2 , 0, 20)
+      });
+      
+      // Assign the keyframes to the animation
+      animation.setKeys(keys);
+      
+      // Apply the animation to the camera
+      camera.animations.push(animation);
+      
+      // Start the animation
+      scene.beginAnimation(camera, 0, 100, true);
+*/
+
+
+
+    // Skybox
+    var skybox = BABYLON.Mesh.CreateBox("skyBox", 1000.0, scene);
+    var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
+    skyboxMaterial.backFaceCulling = false;
+    skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("skybox/skybox", scene);
+    skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+    skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+    skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+    skybox.material = skyboxMaterial;
+
+    var extraGround = BABYLON.Mesh.CreateGround("extraGround", 1000, 1000, 1, scene, false);
+    var extraGroundMaterial = new BABYLON.StandardMaterial("extraGround", scene);
+    extraGroundMaterial.diffuseTexture = new BABYLON.Texture("ground.jpg", scene);
+    extraGroundMaterial.diffuseTexture.uScale = 60;
+    extraGroundMaterial.diffuseTexture.vScale = 60;
+    extraGround.position.y = -2.05;
+    extraGround.material = extraGroundMaterial;
+
+    var ground = BABYLON.Mesh.CreateGroundFromHeightMap("ground", "heightMap.png", 100, 100, 100, 0, 10, scene, false);
+    var groundMaterial = new BABYLON.StandardMaterial("ground", scene);
+    groundMaterial.diffuseTexture = new BABYLON.Texture("ground.jpg", scene);
+    groundMaterial.diffuseTexture.uScale = 6;
+    groundMaterial.diffuseTexture.vScale = 6;
+    groundMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+    ground.position.y = -2.0;
+    ground.material = groundMaterial;
+
+    engine.runRenderLoop(function () {
+        scene.render();
+    });
 }
-const scene = Playground.CreateScene(engine, canvas);
-// RENDER LOOP
-engine.runRenderLoop(() => {
-    scene.render();
-});
-// RESIZE LISTENER
-window.addEventListener("resize", () => {
-    engine.resize();
-});
+
+function mouseMoveHandler(e) {
+    // Get the mouse coordinates relative to the canvas or scene
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+
+    console.log("mousex: ",mouseX);
+    console.log("mousey: ",mouseY);
+
+    let rotationY = mouseX / 420;
+    let rotationX = mouseY/200 *-1;
+
+    camera.rotation.y = rotationY;
+    camera.rotation.x = rotationX;
+}
